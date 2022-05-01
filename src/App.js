94a8fd3c6aae5d2ea27f9e742d5fd993229bcbdc
nhanlnt-96 <nhanlnt@hotmail.com>
2022-05-01 13:12:14 +0700

    complete initial app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import MintBox from "./components/MintBox";
+
+
+function App() {
+  return (
+    <MintBox/>
+  );
+}
+
+export default App;
